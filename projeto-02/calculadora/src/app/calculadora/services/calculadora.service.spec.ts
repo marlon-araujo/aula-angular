c5da9f014f59bdc31202a5962ac9ce00ae15bb8f
Marlon Araújo <marlon_mat@hotmail.com>
2022-07-04 16:43:23 -0300

    [feat] - fazendo testes

--- projeto-02/calculadora/src/app/calculadora/services/calculadora.service.spec.ts
+++ projeto-02/calculadora/src/app/calculadora/services/calculadora.service.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed } from '@angular/core/testing';
+import { inject, TestBed } from '@angular/core/testing';
 
 import { CalculadoraService } from './calculadora.service';
 
@@ -13,4 +13,44 @@ describe('CalculadoraService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  //TESTE SOMA
+  it('deve garantir 1 + 4 = 5',
+    inject([CalculadoraService], (service: CalculadoraService) => {
+      let soma = service.calcular(1, 4, CalculadoraService.SOMA);
+      expect(soma).toEqual(5);
+    })
+  );
+
+  //TESTE SUBTRAÇÃO
+  it('deve garantir 1 - 4 = -3',
+    inject([CalculadoraService], (service: CalculadoraService) => {
+      let soma = service.calcular(1, 4, CalculadoraService.SUBTRACAO);
+      expect(soma).toEqual(-3);
+    })
+  );
+
+  //TESTE DIVISAO
+  it('deve garantir 1 / 4 = 0.25',
+    inject([CalculadoraService], (service: CalculadoraService) => {
+      let soma = service.calcular(1, 4, CalculadoraService.DIVISAO);
+      expect(soma).toEqual(0.25);
+    })
+  );
+
+  //TESTE MULTIPLICACAO
+  it('deve garantir 1 * 4 = 4',
+    inject([CalculadoraService], (service: CalculadoraService) => {
+      let soma = service.calcular(1, 4, CalculadoraService.MULTIPLICACAO);
+      expect(soma).toEqual(4);
+    })
+  );
+
+  //TESTE INVÁLIDO
+  it('deve retornar para operação inválida',
+    inject([CalculadoraService], (service: CalculadoraService) => {
+      let soma = service.calcular(1, 4, '%');
+      expect(soma).toEqual(0);
+    })
+  );
 });
