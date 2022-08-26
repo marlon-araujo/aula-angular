bb6d68cf0d75df5e46a1609d62a3107d4c1342a0
Marlon Araújo <marlon.de.a.teixeira@avanade.com>
2022-08-26 20:34:34 -0300

    [FEAT] criando lista

--- projeto-03/gerenciador-de-tarefas/src/app/tarefas/listar/listar-tarefa.component.ts
+++ projeto-03/gerenciador-de-tarefas/src/app/tarefas/listar/listar-tarefa.component.ts
@@ -11,14 +11,29 @@ export class ListarTarefaComponent implements OnInit {
 
   tarefas: Tarefa[];
 
-  constructor(private tarefaSerive: TarefaService) { }
+  constructor(private tarefaService: TarefaService) { }
 
   ngOnInit(): void {
     this.tarefas = this.listarTodos();
   }
 
   listarTodos(): Tarefa[]{
-    return this.tarefaSerive.listarTodos();
+    return this.tarefaService.listarTodos();
+  }
+
+  remover($event: any, tarefa: Tarefa): void{
+    $event.preventDefault();
+    if(confirm('Deseja remover a tarefa "' + tarefa.nome + '"?')){
+      this.tarefaService.remover(tarefa.id);
+      this.tarefas = this.listarTodos();
+    }
+  }
+
+  alterarStatus(tarefa: Tarefa): void {
+    if(confirm('Deseja alterar o status da tarefa "' + tarefa.nome + '"?')){
+      this.tarefaService.alterarStatus(tarefa.id);
+      this.tarefas = this.listarTodos();
+    }
   }
 
 }
